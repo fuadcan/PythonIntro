6dd83842250444e2f22def836a387454db60af80
fuadcan <fuadcan@gmail.com>
2018-05-01 01:23:26 +0300

    updated files

---
+++ geri_sayim.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+saniye = input("saniye?: ")
+
+saniye = int(saniye)
+
+for i in reversed(range(saniye)):
+    print(i + 1)
+    sleep(1)
+
+
+print("zı"+ "r"*10)
+
--- papatya_fali_v2.py
+++ papatya_fali_v2.py
@@ -8,16 +8,18 @@ sleep(1)
 
 yaprak_sayisi = randint(20,30)
 son_durum = ''
+durumlar = ['seviyor','sevmiyor']
 
 for i in range(0,yaprak_sayisi):
-    if i % 2 == 0:
-        son_durum='seviyor'
-        print(son_durum)
-        sleep(.5)
-    else:
-        son_durum = 'sevmiyor'
-        print(son_durum)
-        sleep(.5)
+    son_durum = durumlar[i % 2]
+    print(son_durum, "iterasyon: ", i, "mod: ", i % 2)
+    sleep(.5)
+
+sleep(2)
+print("hmmm")
+sleep(1)
+print("hmmmmmm")
+sleep(1)
 
 if son_durum=='sevmiyor':
     print("%s seni sevmiyor!! Ahahahahahha" % isim)
