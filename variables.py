997ba4034074eecf914351941a1b137ebd42c0c2
fuadcan <fuadcan@gmail.com>
2018-05-02 00:48:54 +0300

    added 2 modules

---
+++ variables.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+#### Sayılar
+x = 3
+print(x)
+type(x) # int
+
+y = x * 2
+print(y)
+type(y) # int
+
+x = 3.
+type(x) # float
+
+y = x * 2
+print(y)
+type(y) # float
+
+### Temel Matematik İşlemleri
+x = 3 + 2
+x = 3 - 2
+x = 3 * 2
+x = 3 / 2 # !!!!!!!!!!!!!!!!!
+
+x = 3 ** 2
+
+# İşlem Öncelikleri
+3 * 2 + 4
+3 * (2 + 4)
+3 ** 2 * 4
+3 ** (2 * 4)
+
+6 - 4 - 1
+
+
+# Metinler
+a = 'adnan'
+print(a)
+type(a)
+
+## Listeler
+lst = list()
+lst = []
+
+lst = [1,2,3]
+lst = ['a','b','c']
+lst = ['a',2,20,'d']
+
+lst2 = ['a',2,20, [1,2,3,20,'a'] ]
