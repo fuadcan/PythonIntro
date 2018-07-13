4e9b635a41bef1fea9072d1d2a08e19134ac87d2
fuadcan <fuadcan@gmail.com>
2018-07-13 09:36:33 +0300

    updated regex and pandas

---
+++ ders1.py
@@ -0,0 +1,14 @@
+x = 3
+
+x * 2
+
+print(x * 6)
+
+babamin_adi = 'ibrahim'
+
+print(babamin_adi)
+
+
+while True:
+    answer = input('Sana bonservis taklidi yapayim mi?')
+    print('oyle %s demekle olmaz' % answer)
